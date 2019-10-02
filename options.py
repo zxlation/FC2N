da37c1ee51447d952dbc44f41f0e2448b47aa219
Xiaole Zhao <1023270208@qq.com>
2019-10-02 19:02:37 +0800

    Update options.py

--- options.py
+++ options.py
@@ -7,11 +7,8 @@ Created on Sat Mar  3 19:56:38 2018
 from __future__ import absolute_import
 from __future__ import division
 import argparse
-
 parser = argparse.ArgumentParser()
-#################################################################################################
-#                                            Train                                              #
-#################################################################################################
+
 parser.add_argument('--batch_size', type = int, default = 16,
                     help = 'Number of examples to process in a batch.')
 parser.add_argument('--patch_size', type = int, default = 48,
@@ -31,20 +28,15 @@ parser.add_argument('--train_from_exist', type = bool, default = False,
 parser.add_argument('--exist_model_dir', type = str, default = 'max_logs/',
                     help = 'Directory where to load pretrianed models.')
 
-#################################################################################################
-#                                              Valid                                            #
-#################################################################################################
+
 parser.add_argument('--valid_log_dir', type = str, default = 'valid_logs/',
                     help = 'Directory where to write event logs and checkpoints.')
 parser.add_argument('--image_save_dir', type = str, default = 'ret_images/',
                     help = 'Directory where to save predicted HR images.')
 parser.add_argument('--valid_model_dir', type = str, default = 'train_logs/',
                     help = 'Directory where the model that needs evaluation is saved.')
-
 #%%
 params = parser.parse_args()
 
-if __name__ == '__main__':
-    print("batch size = %d" % params.batch_size)
     
 
